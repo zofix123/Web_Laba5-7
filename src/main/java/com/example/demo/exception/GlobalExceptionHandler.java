da97bee51631b9714c3598a76f093a1c8ed0807f
Vlad <ytvctyg@mail.ru>
2025-12-22 00:33:35 +0300

    Добавил серверное время на всех страницах

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -3,24 +3,9 @@ package com.example.demo.exception;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ModelAttribute;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    @ModelAttribute
-    public void addCommonAttributes(Model model) {
-        // текущее время сервера
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        model.addAttribute("serverTime", now.format(formatter));
-
-        model.addAttribute("serverDate", now.format(DateTimeFormatter.ofPattern("yyyy.MM.dd")));
-        model.addAttribute("serverTimeOnly", now.format(DateTimeFormatter.ofPattern("HH:mm:ss")));
-    }
 
     @ExceptionHandler(EmailAlreadyExistsException.class)
     public String handleEmailAlreadyExists(EmailAlreadyExistsException e, Model model) {
